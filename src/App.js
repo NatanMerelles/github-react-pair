a9ccbcef805d4d6b76bebcdaff4754370517cf58
Natan Merelles <natanmerelles@gmail.com>
2021-07-03 18:37:26 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Body = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Repo = styled.div`
+  width: 100px;
+  padding: 20px;
+  border: 1px solid #b3b3b3;
+  border-radius: 25px;
+`;
+
 function App() {
+  const [profile, setProfile] = useState();
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/natanmerelles')
+      .then((result) => result.json())
+      .then((dados) => setProfile(dados));
+  }, []);
+
+  useEffect(() => {
+    if (profile) {
+      fetch(profile.repos_url)
+        .then((result) => result.json())
+        .then((dados) => setRepos(dados));
+    }
+  }, [profile]);
+
+  console.log({ profile });
+  console.log({ repos });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Body>
+        {profile ? profile.login : 'Um nome'}
+        {
+          repos.map((repo) => (
+            <Repo>{repo.name}</Repo>
+          ))
+        }
+      </Body>
     </div>
   );
 }
